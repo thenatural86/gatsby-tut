8127850346ed845ce272ac07d4e664661fc34b1a
thenatural86 <philip.smalls86@gmail.com>
2021-01-28 14:41:39 -0500

    navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       Yolo world!
       <div>
         <Link to="/blog"> blog page</Link>
