b7441783c5a743bd8a49d03636c11d5eca7ce2cd
thenatural86 <philip.smalls86@gmail.com>
2021-01-28 14:07:46 -0500

    link tag

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,10 @@
 import React from "react"
-
+import { Link } from "gatsby"
 const blog = () => {
   return (
     <div>
       <h1>blog page</h1>
+      <Link to="/">homepage</Link>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Yolo world!</div>
+  return (
+    <div>
+      Yolo world!
+      <div>
+        <Link to="/blog"> blog page</Link>
+      </div>
+      <div>
+        <a target="_blank" href="https://www.gatsbyjs.com">
+          gatsby docs
+        </a>
+      </div>
+    </div>
+  )
 }
