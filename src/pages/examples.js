84ac7c7221cd261cd874cf787faff18bf5f48dde
thenatural86 <philip.smalls86@gmail.com>
2021-01-29 15:30:14 -0500

    old static query

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import Header from "../example/Header"
+import HeaderStatic from "../example/HeaderStatic"
 
 const examples = () => {
   return (
     <Layout>
       <h1>Examples page</h1>
       <Header />
+      <HeaderStatic />
     </Layout>
   )
 }
