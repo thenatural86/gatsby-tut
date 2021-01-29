6b2edd753e71ac466edafab37a9389860a6f417e
thenatural86 <philip.smalls86@gmail.com>
2021-01-28 20:06:15 -0500

    styles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
+import styles from "../components/blog.module.css"
 
 const blog = () => {
   return (
     <Layout>
-      <h1>blog page</h1>
+      <div className={styles.page}>
+        <h1>blog page</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          tenetur ullam molestiae deleniti eligendi aliquam velit dicta
+          repellat. Nihil qui rerum nesciunt est quas non.
+        </p>
+      </div>
     </Layout>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,10 +1,20 @@
 import React from "react"
 import Layout from "../components/layout"
 
+import styles from "../components/products.module.css"
+
+// console.log(styles)
 const products = () => {
   return (
     <Layout>
-      <h1>products page</h1>
+      <div className={styles.page}>
+        <h1>products page</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          iure sint laborum, libero suscipit quo rem ab? Nobis temporibus saepe
+          minus repudiandae, delectus qui accusantium.
+        </p>
+      </div>
     </Layout>
   )
 }
