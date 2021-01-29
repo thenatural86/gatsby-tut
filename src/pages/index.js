b79f04478d0fe4aa5c693e201aa3432095c77725
thenatural86 <philip.smalls86@gmail.com>
2021-01-28 20:21:23 -0500

    styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
+import { ExampleButton } from "../components/button"
 
 export default function Home() {
   return (
     <Layout>
       <h1 style={{ color: "red", textTransform: "uppercase" }}>Yolo World</h1>
+
+      <ExampleButton>click er roo</ExampleButton>
     </Layout>
   )
 }
