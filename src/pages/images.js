1abe2942e38b4b6db0837ac0ca2caef5e5219987
thenatural86 <philip.smalls86@gmail.com>
2021-01-30 13:58:42 -0500

    gatsby imaged setup

---
+++ src/pages/images.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import Images from "../example/Images"
+
+const images = () => {
+  return (
+    <Layout>
+      <Images />
+    </Layout>
+  )
+}
+
+export default images
