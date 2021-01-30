19aec357fa57ad53e5a1d1aeebd97e5b8d6719a3
thenatural86 <philip.smalls86@gmail.com>
2021-01-29 20:05:07 -0500

    page query

--- src/example/Header.js
+++ src/example/Header.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 const getData = graphql`
-  query FirstQuery {
+  query {
     site {
       info: siteMetadata {
         author
@@ -30,8 +30,8 @@ const Header = () => {
 
   return (
     <div>
-      {/* <h1>title : {data.site.siteMetadata.title}</h1>
-      <h1>name : {data.site.siteMetadata.name}</h1> */}
+      {/* <h1>title : {data.site.siteMetadata.title}</h1> */}
+      {/* <h1>name : {data.site.siteMetadata.name}</h1> */}
       <h1>title : {title}</h1>
       <h1>name : {name}</h1>
     </div>
--- src/pages/examples.js
+++ src/pages/examples.js
@@ -2,15 +2,38 @@ import React from "react"
 import Layout from "../components/layout"
 import Header from "../example/Header"
 import HeaderStatic from "../example/HeaderStatic"
+import { graphql } from "gatsby"
 
-const examples = () => {
+const examples = ({ data }) => {
+  const {
+    site: {
+      info: { author },
+    },
+  } = data
   return (
     <Layout>
       <h1>Examples page</h1>
       <Header />
       <HeaderStatic />
+      <h5>author : {author}</h5>
     </Layout>
   )
 }
 
+export const data = graphql`
+  query MyQuery {
+    site {
+      info: siteMetadata {
+        author
+        data
+        description
+        title
+        person {
+          age
+          name
+        }
+      }
+    }
+  }
+`
 export default examples
