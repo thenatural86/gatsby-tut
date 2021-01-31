269657a0e8af6ffbf750e3111ad909fc85680a9e
thenatural86 <philip.smalls86@gmail.com>
2021-01-31 13:10:08 -0500

    initial product render

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,12 +1,50 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import styles from "../components/products.module.css"
+import Image from "gatsby-image"
+import { Link } from "gatsby"
 
-const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
+const ComponentName = ({ data }) => {
+  const {
+    allContentfulProduct: { nodes: products },
+  } = data
+  // console.log(products)
+  return (
+    <Layout>
+      <section className={styles.page}>
+        {products.map(product => {
+          {
+            /* console.log(product) */
+          }
+          const {
+            title,
+            id,
+            image: { fluid },
+            price,
+            slug,
+          } = product
+          return (
+            <article key={id}>
+              {title}
+              <Image fluid={fluid} alt={title} />
+              <h3>
+                {title} <span>${price}</span>
+              </h3>
+              <Link to={`/products/${slug}`}>more details</Link>
+            </article>
+          )
+        })}
+      </section>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   {
     allContentfulProduct {
       nodes {
+        id
         price
         title
         slug
